cc978ed1fa80deae52dd5fc17204e184b7695d00
Priyam8878 <112754436+Priyam8878@users.noreply.github.com>
2023-06-27 15:55:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	const name = document.childNodes("form");
+	const firstName = name[0].value;
+	const secondName = name[1].value;
+	alert(firstName+" "+secondName);
+	
+	
 
 }
